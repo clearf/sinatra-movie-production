03998f4244f7b3981508ca98d793f805cb471338
Ben Israeli <jisraeli@gmail.com>
2013-07-01 16:21:57 -0400

    created tables people,tasks, movies

---
+++ movieproduction.sql
@@ -0,0 +1,28 @@
+DROP TABLE TODOS
+DROP TABLE MOVIES
+DROP TABLE PEOPLE
+
+CREATE TABLE PEOPLE
+(ID SERIAL PRIMARY KEY,
+  name VARCHAR(127),
+  title VARCHAR(127),
+  phone VARCHAR(15)
+  );
+
+CREATE TABLE TASKS
+(ID SERIAL PRIMARY KEY,
+  task VARCHAR(127),
+  description TEXT,
+  person_id INT references people(id),
+  movie_id INT references movies(id),
+  completed BOOLEAN,
+);
+
+CREATE TABLE MOVIES
+(ID SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  director_id INT references movies(id),
+  image TEXT
+);
+
+
