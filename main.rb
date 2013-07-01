f6296a51f289cff1811fd4c49af3db3fd8d9c364
Beryl <beryl.schragger@gmail.com>
2013-07-01 17:00:45 -0400

    Add basic pages for showing movies, people, and tasks.

--- main.rb
+++ main.rb
@@ -12,11 +12,31 @@ helpers do
   end
 end
 
-# index page
+# index page - immediately redirects to list of movies
 get '/' do
   redirect to '/movies'
 end
 
+# page with list of movies - basically index page
 get '/movies' do
 end
 
+# individual movie page
+get '/movies/:id' do
+end
+
+# page with list of people
+get '/people' do
+end
+
+# individual person page
+get 'people/:id' do
+end
+
+# page with all tasks - may be changed once flow is determined
+get '/tasks' do
+end
+
+# individual task page - may be changed once flow is determined
+get '/tasks/:id' do
+end
