085ef68bedca6e57234c3e281a7485eb3614fb0a
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-07-01 16:30:40 -0400

    List of tasks created

---
+++ main.rb
@@ -0,0 +1,32 @@
+require 'pg'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry'
+
+helpers do
+def run_sql(sql)
+  db = PG.connect(dbname: 'production', host:'localhost')
+  result = db.exec(sql)
+  db.close
+  result
+end
+end
+
+#Show list of tasks
+get '/tasks' do
+  sql = "SELECT * from tasks"
+  @tasks = run_sql(sql)
+  erb:tasks
+end
+
+#Show list of movies
+
+#Show list of contacts
+
+#Add new task
+#Add new movie
+#Add new contact
+
+#Edit task
+#Edit movie
+#Edit contact
