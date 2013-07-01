100fd0227b8e88e9f7ecfae27ef19b34322bd2b4
Beryl <beryl.schragger@gmail.com>
2013-07-01 19:40:11 -0400

    Add list of movies and people to respective pages.

--- main.rb
+++ main.rb
@@ -19,24 +19,34 @@ end
 
 # page with list of movies - basically index page
 get '/movies' do
+  sql = 'select * from movies'
+  @movies = run_sql(sql)
+  erb :movies
 end
 
 # individual movie page
 get '/movies/:id' do
+  erb :movie
 end
 
 # page with list of people
 get '/people' do
+  sql = 'select * from people'
+  @people = run_sql(sql)
+  erb :people
 end
 
 # individual person page
 get 'people/:id' do
+  erb :person
 end
 
 # page with all tasks - may be changed once flow is determined
 get '/tasks' do
+  erb :todos
 end
 
 # individual task page - may be changed once flow is determined
 get '/tasks/:id' do
+  erb :todo
 end
--- movies.sql
+++ movies.sql
@@ -7,14 +7,27 @@ create table movies (
 
 create table people (
   id serial primary key,
-  name varchar(255),
-  job varchar(255)
+  name varchar(255)
 );
 
 create table tasks (
   id serial primary key,
   name varchar(255),
   description varchar(255),
+  done boolean default false,
   person int references people(id),
   movie int references movies(id)
 );
+
+insert into movies (name, director, release_date) values ('A Scanner Darkly', 'Richard Linklater', 2006);
+insert into movies (name, director, release_date) values ('Iron Man', 'Jon Favreau', 2008);
+
+
+insert into people (name) values ('Richard Linklater');
+insert into people (name) values ('Keanu Reeves');
+insert into people (name) values ('Jon Favreau');
+insert into people (name) values ('Robert Downey Jr');
+insert into people (name) values ('Jeff Bridges');
+insert into people (name) values ('Joey Bagadonuts');
+
+# insert into tasks (name, description, person, movie) values
