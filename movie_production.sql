16b245d792cd775b93b1978d2f15720063dc163d
nicklombardi <nicklombardi@gmail.com>
2013-07-01 17:17:12 -0400

    updated movie_production.sql

--- movie_production.sql
+++ movie_production.sql
@@ -6,26 +6,34 @@ DROP TABLE movies;
 CREATE TABLE movies
 (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
-  release VARCHAR(255),
+  movie_name VARCHAR(255),
+  release INT,
   director VARCHAR(255)
 );
 
 CREATE TABLE tasks
 (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
+  task_name VARCHAR(255),
   description VARCHAR(255),
-  contact INT REFERENCES people(id),
-  movie INT REFERENCES movies(id)
+  -- contact_id INT REFERENCES people(id),
+  movie_id INT REFERENCES movies(id)
 );
 
 CREATE TABLE people
 (
   id SERIAL PRIMARY KEY,
-  movie INT REFERENCES movies(id),
-  release VARCHAR REFERENCES movies(release),
-  director VARCHAR REFERENCES movies(director),
-  task INT REFERENCES tasks(id)
+  person VARCHAR(255),
+  movie_id INT REFERENCES movies(id),
+  -- release INT REFERENCES movies(release),
+  -- director VARCHAR REFERENCES movies(director),
+  task_id INT REFERENCES tasks(id)
 );
+
+
+INSERT INTO movies (movie_name, release, director) VALUES ('Eternal Sunshine of the Spotless Mind', 2004, 'Michel Gondry');
+
+INSERT INTO tasks (task_name, description, movie_id) VALUES ('Rewrite beach scene', 'Add sand is overrated line', 1);
+
+INSERT INTO people (person, movie_id, task_id) VALUES ('Charlie Kaufman', 1, 1);
 
