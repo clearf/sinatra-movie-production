203c0b1ef5f9ee05955666aa53dcc8026bf538fb
tommyb67 <tommybrennan@gmail.com>
2013-07-01 19:00:44 -0400

    I am able to display all of the todos

--- main.rb
+++ main.rb
@@ -2,9 +2,27 @@ require 'pg'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+helpers do
+  # This helps run sql commands
+  def run_sql(sql)
+    db = PG.connect(dbname: 'sinatra_movies', host: 'localhost')
+    result = db.exec(sql)
+    db.close
+    result
+  end
+end
 
+
+# root
 get '/' do
   erb :index
+end
+
+
+get '/todos' do
+  sql = "SELECT * FROM tasks"
+  @tasks = run_sql(sql)
+  erb :todos
 end
 
 
