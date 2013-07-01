f5683cb8f68ac0468301d73c65d237151a5e1c3d
mvanger <mvanger@nyu.edu>
2013-07-01 16:28:57 -0400

    Wrote a sql function

--- main.rb
+++ main.rb
@@ -2,6 +2,15 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pg'
 
+helpers do
+  def run_sql(sql)
+    db = PG.connect(dbname: 'westeros', host: 'localhost')
+    result = db.exec(sql)
+    db.close
+    result
+  end
+end
+
 get '/' do
   erb :index
 end
