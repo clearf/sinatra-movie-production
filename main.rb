c5282acd1f6f4d438092444c286085838e8246eb
Christopher Wright <cmwright33@gmail.com>
2013-07-01 17:21:24 -0400

    created todos interation, todos_edit, todos create, added yieldto layout

--- main.rb
+++ main.rb
@@ -11,25 +11,88 @@ helpers do
   end
 end
 
-# this provides you with the index page
+# this provides you with the index page and will rever
 get '/' do
   erb :index
 end
-# Todo Tasks
+
+#       #
+# Tasks #
+#       #
+
+# gets all the tasks
+
+get '/todos' do
+
+erb :todos
+end
+
+# gives each individual task
+
+get '/:id/todo' do
+
+erb :todo
+end
 
 # create and assign new todos
-
 get '/create_todo' do
+
   erb :create_todo
 end
 
+# posts the task back to
 post '/create_todo' do
+  task = params[:task]
+  task_description = params[:task_description]
+  sql = "INSERT INTO todo (task, task_description, person_id, movie_id) VALUES ('#{task}','#{task_description}', #{person_id}, #{movie_id});"
+  @todos = run_sql(sql)
+  erb :todos
+end
+
+
+
+
+#         #
+# People  #
+#         #
+
+# get all the people
+
+get'/people' do
+
+  erb :people
+end
+
+# get each individual person
+
+get'/person' do
+
+  erb :person
+end
+
+# creates an new person and assigns
+get '/create_person' do
+
+  erb :create_person
+end
+
+#posts the person back to people
+post '/create_person' do
+
+  erb :people
+end
+
+get '/people/:id/edit/' do
 
 end
 
 
-# People
+#        #
+# Movies #
+#        #
 
 
+get'/movies/' do
 
-# Movies
+end
+
