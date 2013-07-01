b9eb080babdde419cf5ec2d667d24ac3b9f6170c
nicklombardi <nicklombardi@gmail.com>
2013-07-01 17:55:39 -0400

    created new movie functionality

--- main.rb
+++ main.rb
@@ -18,13 +18,32 @@ get '/' do
 end
 
 get '/movies' do
+  sql = "SELECT * FROM movies"
+  @movies = run_sql(sql)
   erb :movies
 end
 
+get '/movies/new' do
+  erb :new_movie
+end
+
+post '/movies/new' do
+  movie_name = params[:movie_name]
+  release = params[:release]
+  director = params[:director]
+  sql = "INSERT INTO movies (movie_name, release, director) VALUES ('#{movie_name}',#{release},'#{director}');"
+  run_sql(sql)
+  redirect to '/movies'
+end
+
 get '/todos' do
+  sql = "SELECT * FROM tasks"
+  @tasks = run_sql(sql)
   erb :todos
 end
 
 get '/people' do
+  sql = "SELECT * FROM people"
+  @people = run_sql(sql)
   erb :people
 end
