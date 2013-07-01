3f18c4c9c4e0ffa48ec11b3deeb29603bd460673
nicklombardi <nicklombardi@gmail.com>
2013-07-01 17:29:15 -0400

    added helper in main

--- main.rb
+++ main.rb
@@ -3,3 +3,28 @@ require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+helpers do
+  # This helps us run SQL commands
+  def run_sql(sql)
+    db = PG.connect(dbname: 'movie_production', host: 'localhost')
+    result = db.exec(sql)
+    db.close
+    result
+  end
+end
+
+get '/' do
+  erb :index
+end
+
+get '/movies' do
+  erb :movies
+end
+
+get '/todos' do
+  erb :todos
+end
+
+get '/people' do
+  erb :people
+end
