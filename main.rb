826dec26600b6be0d5ea0b0730df17e97755b212
alicem <alice.mottola@gmail.com>
2013-07-01 22:40:04 -0400

    Add all movie editing functionality

--- main.rb
+++ main.rb
@@ -25,6 +25,21 @@ get '/' do
   erb :tasks
 end
 
+get '/tasks/:id' do
+  id = params[:id]
+
+  sql = "SELECT * FROM tasks WHERE id = #{id}"
+  @task = run_sql(sql).first
+
+  sql = "SELECT name FROM people WHERE id = #{@task['person_id']}"
+  @person = run_sql(sql).first
+
+  sql = "SELECT title FROM movies WHERE id = #{@task['movie_id']}"
+  @movie = run_sql(sql).first
+
+  erb :task
+end
+
 post '/new_task' do
   task = params[:task]
   description = params[:description]
@@ -32,6 +47,39 @@ post '/new_task' do
   movie_id = params[:movie_id]
 
   sql = "INSERT INTO tasks (task, description, person_id, movie_id) VALUES ('#{task}', '#{description}', #{person_id}, #{movie_id})"
+  run_sql(sql)
+
+  redirect to('/')
+end
+
+get '/edit_task/:id' do
+  id = params[:id]
+
+  sql = "SELECT * FROM tasks WHERE id = #{id}"
+  @task = run_sql(sql).first
+
+  sql = "SELECT id, name FROM people"
+  @people = run_sql(sql)
+
+  sql = "SELECT id, title FROM movies"
+  @movies = run_sql(sql)
+
+  erb :edit_task
+end
+
+post '/edit_task/:id' do
+  id = params['id']
+  task = params['task']
+  description = params['description']
+  person_id = params['person_id']
+  movie_id = params['movie_id']
+
+  completed = false
+  if params['completed'].chomp == 'y'
+    completed = true
+  end
+
+  sql = "UPDATE tasks SET (task, description, person_id, movie_id, completed) = ('#{task}', '#{description}', #{person_id}, #{movie_id}, #{completed}) WHERE id = #{id}"
   run_sql(sql)
 
   redirect to('/')
@@ -67,11 +115,57 @@ post '/new_movie' do
   redirect to('/movies')
 end
 
+get '/movies/:id' do
+  id = params[:id]
+
+  sql = "SELECT * FROM movies WHERE id = #{id}"
+  @movie = run_sql(sql).first
+
+  sql = "SELECT name FROM people WHERE id = #{@movie['director_id']}"
+  @director = run_sql(sql).first
+
+  erb :movie
+end
+
+get '/edit_movie/:id' do
+  id = params[:id]
+
+  sql = "SELECT * FROM movies WHERE id = #{id}"
+  @movie = run_sql(sql).first
+
+  sql = "SELECT id, name FROM people"
+  @people = run_sql(sql)
+
+  erb :edit_movie
+end
+
+post '/edit_movie/:id' do
+  id = params[:id]
+  title = params[:title]
+  director_id = params['director_id']
+  year = params['year']
+  image = params['image']
+
+  sql = "UPDATE movies SET (title, director_id, year, image) = ('#{title}', '#{director_id}', '#{year}', '#{image}') WHERE id = #{id}"
+  run_sql(sql)
+
+  redirect to('/movies')
+end
+
 get '/people' do
   sql = "SELECT * FROM people"
   @people = run_sql(sql)
 
   erb :people
+end
+
+get '/people/:id' do
+  id = params[:id]
+
+  sql = "SELECT * FROM people WHERE id = #{id}"
+  @person = run_sql(sql).first
+
+  erb :person
 end
 
 post '/new_person' do
@@ -84,5 +178,30 @@ post '/new_person' do
 
   redirect to('/people')
 end
+
+get '/edit_person/:id' do
+  id = params[:id]
+
+  sql = "SELECT * FROM people WHERE id = #{id}"
+  @person = run_sql(sql).first
+
+  erb :edit_person
+end
+
+post '/edit_person/:id' do
+  id = params['id']
+  name = params['name']
+  title = params['title']
+  phone = params['phone']
+
+  sql = "UPDATE people SET (name, title, phone) = ('#{name}', '#{title}', '#{phone}') WHERE id = #{id}"
+  run_sql(sql)
+
+  redirect to('/people')
+end
+
+
+
+
 
 
--- movie_production_manager.sql
+++ movie_production_manager.sql
@@ -10,14 +10,14 @@
 --   phone VARCHAR(16)
 -- );
 
--- CREATE TABLE MOVIES
--- (
---   id SERIAL PRIMARY KEY,
---   title VARCHAR(255),
---   director_id INT REFERENCES movies(id),
---   year VARCHAR(7),
---   image TEXT
--- );
+CREATE TABLE MOVIES
+(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  director_id INT REFERENCES people(id),
+  year VARCHAR(7),
+  image TEXT
+);
 
 -- CREATE TABLE TASKS
 -- (
@@ -29,8 +29,8 @@
 --   completed BOOLEAN DEFAULT FALSE
 -- );
 
-INSERT INTO people (name, title, phone) VALUES ('Asha Greyjoy', 'Director', '201-396-8258');
-INSERT INTO people (name, title, phone) VALUES ('Andrei Mendeleev', 'Director', '617-242-3632');
+-- INSERT INTO people (name, title, phone) VALUES ('Asha Greyjoy', 'Director', '201-396-8258');
+-- INSERT INTO people (name, title, phone) VALUES ('Andrei Mendeleev', 'Director', '617-242-3632');
 
 INSERT INTO movies (title, director_id, year, image) VALUES ('The Cat and the Candle', 1, '2014', 'http://andreapaesante.com/wp-content/uploads/2011/02/smat-cat-with-candle.jpg');
 INSERT INTO movies (title, director_id, year, image) VALUES ('Psychodrama in the Afternoon', 2, '2013', 'http://www.thebridgepai.com/wp-content/deren-1.jpg');
