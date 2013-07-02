ab5085a356e7668a4ad11c8ff31acf929441be26
Christopher Wright <cmwright33@gmail.com>
2013-07-02 02:00:50 -0400

    whoops missed a few commits, syntax and other errors in many files, finally added the selector option on the create_todo.erb, add a bit of css styling

--- main.rb
+++ main.rb
@@ -1,17 +1,18 @@
 require 'pry'
 require 'sinatra'
-require 'sinatra/reloader'
+require 'pg'
+require 'sinatra/reloader' if development?
 
 helpers do
   def run_sql(sql)
-    db = PG.connect(dbname: 'movie_production', host: 'localhost')
+    db = PG.connect(:dbname => 'movie_production', :host => 'localhost')
     result = db.exec(sql)
     db.close
     result
   end
 end
 
-# this provides you with the index page and will rever
+# this provides you with the index page that links to each seperate page
 get '/' do
   erb :index
 end
@@ -23,23 +24,26 @@ end
 # gets all the tasks
 
 get '/todos' do
-  sql = "select * from todos"
-  run_sql(sql)
+  sql = "select * from todo"
+  @todos = run_sql(sql)
 erb :todos
 end
 
 # gives each individual task
 
-get '/:id/todo' do
+get '/todo/:id' do
   id = params[:id]
-  sql = "select * from todos where id = #{id}"
-  @todo= run_sql(sql)
+  sql = "select * from todo where id = #{id}"
+  @todo = run_sql(sql).first
 erb :todo
 end
 
-# create and assign new todos
+# create and assign new todo
 get '/create_todo' do
-
+  sql = "select id, name from people"
+  @people = run_sql(sql)
+  sql = "select id, movie_name from movies"
+  @movies = run_sql(sql)
   erb :create_todo
 end
 
@@ -71,7 +75,7 @@ get'/person/:id' do
   name = params[:name]
   occupation = params[:occupation]
   sql = "select * from people where id = #{'id'}"
-  @person = run_sql(sql)
+  @person = run_sql(sql).first
   erb :person
 end
 
@@ -80,7 +84,7 @@ get '/create_person' do
   erb :create_person
 end
 
-#posts the person back to people
+# posts the person back to people
 post '/create_person' do
   id = params[:id]
   name = params[:name]
@@ -90,9 +94,9 @@ post '/create_person' do
 end
 
 # gives you the person information
-get '/people/:id/edit/' do
+get '/person/:id/edit/' do
   sql = "select * from people where id = #{id}"
-  @people = run_sql(sql)
+  @person = run_sql(sql).first
 end
 
 
@@ -100,18 +104,23 @@ end
 # Movies #
 #        #
 
+# gets the movie
 
-get'/movies/' do
-  sql = "select * from people"
+get '/movies' do
+  sql = "select * from movies"
   @movies = run_sql(sql)
   erb :movies
 end
 
+# gets single movie with information
+
 get '/movies/:id' do
-  name = params[:name]
-  occupation = params[:occupation]
-  sql = "insert into movies (movie_name, ) values ('#{name}', '#{occupation}')"
-  erb :people
+  movie_name = params[:movie_name]
+  release_date = params[:release_date]
+  director = params[:director]
+  sql =  "select * from movies where id = #{'id'}"
+  @movie = run_sql(sql).first
+  erb :movie
 end
 
 
--- sinatra_movie.sql
+++ sinatra_movie.sql
@@ -6,39 +6,39 @@ create database movie_production;
   (
   id serial primary key,
   name varchar (20),
-  occupation varchar(30),
-  )
+  occupation varchar(30)
+  );
 
 
-create table movie
+create table movies
   (
   id serial primary key,
   movie_name varchar(40),
   release_date int,
   director varchar(40)
-  )
+  );
 
 create table todo
  (
-  id serial primary key;
-  task varchar(15);
-  task_description varchar(100);
-  person_id int references person(id)
-  movie_id int references movie(id)
-)
+  id serial primary key,
+  task varchar(15),
+  task_description varchar(100),
+  person_id int references people(id),
+  movie_id int references movies(id)
+);
 
 
 --insert into people
 
-insert into people (name, occupation) values ('Bob', 'PA')
-insert into people (name, occupation) values ('Suzy', 'Key Grip')
+insert into people (name, occupation) values ('Bob', 'PA');
+insert into people (name, occupation) values ('Suzy', 'Key Grip');
 
 --insert into movie
 
-insert into movie (movie_name, release_date, director) values ('Dumb and Dumber', 1996, "Farley Brothers")
-insert into movie (movie_name, release_date, director) values ('Hunt for Red October', 1991, "No idea")
+insert into movies (movie_name, release_date, director) values ('Dumb and Dumber', 1996, 'Farley Brothers');
+insert into movies (movie_name, release_date, director) values ('Hunt for Red October', 1991, 'No idea');
 
 --insert into todo
 
-insert into todo (task, task_description, person_id, movie_id) values ('getting coffee', 'duuuhhh, that is why your still a PA', 1, 1 )
-insert into todo (task, task_description, person_id, movie_id) values ('grip things', 'Well... sombody has to do it right?', 2, 2)
+insert into todo (task, task_description, person_id, movie_id) values ('getting coffee', 'duuuhhh, that is why your still a PA', 1, 1 );
+insert into todo (task, task_description, person_id, movie_id) values ('grip things', 'Well... sombody has to do it right?', 2, 2);
