6fc9ab9effbf6459d46acd4b3ae636890a499d99
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-07-02 17:11:34 -0400

    Added contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130702210656) do
+ActiveRecord::Schema.define(version: 20130702211027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "movies", force: true do |t|
     t.string   "name"
