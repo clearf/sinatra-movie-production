f28e9314df35871ea249c74018fb509eb5048e4b
Beryl <beryl.schragger@gmail.com>
2013-07-01 21:54:18 -0400

    Add pages and functions for creating new movies and people.

--- main.rb
+++ main.rb
@@ -25,13 +25,26 @@ get '/movies' do
 end
 
 # individual movie page
-get '/movie/:id' do
+get '/movies/:id' do
   id = params[:id]
   sql_movie = "select * from movies where id = #{id}"
   @movie = run_sql(sql_movie).first
   sql_tasks = "select * from tasks where movie = #{id}"
   @tasks = run_sql(sql_tasks)
   erb :movie
+end
+
+get '/add_movie' do
+  erb :add_movie
+end
+
+post '/add_movie' do
+  name = params[:name]
+  director = params[:director]
+  release_date = params[:release_date]
+  sql = "insert into movies (name, director, release_date) values ('#{name}', '#{director}', #{release_date})"
+  run_sql(sql)
+  redirect to '/movies'
 end
 
 # page with list of people
@@ -42,13 +55,24 @@ get '/people' do
 end
 
 # individual person page
-get '/person/:id' do
+get '/people/:id' do
   id = params[:id]
   sql_person = "select * from people where id = #{id}"
   @person = run_sql(sql_person).first
   sql_tasks = "select * from tasks where person = #{id}"
   @tasks = run_sql(sql_tasks)
   erb :person
+end
+
+get '/add_person' do
+  erb :add_person
+end
+
+post '/add_person' do
+  name = params[:name]
+  sql = "insert into people (name) values ('#{name}')"
+  run_sql(sql)
+  redirect to '/people'
 end
 
 # page with all tasks - may be changed once flow is determined
@@ -58,5 +82,6 @@ end
 
 # individual task page - may be changed once flow is determined
 get '/task/:id' do
+  id = params[:id]
   erb :todo
 end
