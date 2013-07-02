22a631fd0b67c09cadf5a3d84cd8ca896acb3935
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-07-02 01:45:19 -0400

    Added the functinality to add, edit, and delte a todo

--- main.rb
+++ main.rb
@@ -192,28 +192,80 @@ end
 get '/todos' do
   sql_get_todos = "SELECT * FROM tasks"
   @got_todos = run_sql(sql_get_todos)
-  erb :todos
-end
 
-get '/todos' do
+  #LOAD OTHER TABLES FOR SECONDARY INFO
+  sql_get_people = "SELECT * FROM people"
+  @got_people = run_sql(sql_get_people)
+
+
+  sql_get_todos = "SELECT * FROM tasks"
+  @got_todos = run_sql(sql_get_todos)
+
   erb :todos
 end
 
 # HERE USER CAN ADD A NEW TASKS
 get '/todos/new' do
+  sql_get_movies = "SELECT * FROM movies"
+  @got_movies = run_sql(sql_get_movies)
+
+  sql_get_people = "SELECT * FROM people"
+  @got_people = run_sql(sql_get_people)
+
   erb :new_todo
 end
 
 post '/todos/new' do
-  # name = params[:name].capitalize
-  # title = params[:title].capitalize
-  # phone = params[:phone]
-  # idiot = params[:idiot]
+  todo = params[:todo].capitalize
+  note = params[:note].capitalize
+  assigned_to = params[:assigned_to]
+  related_to = params[:related_to]
 
-  # sql_add_person = "INSERT INTO people (name, title, phone, idiot) VALUES ('#{name}', '#{title}', '#{phone}', '#{idiot}')"
-  # run_sql(sql_add_person)
+  sql_add_todo = "INSERT INTO tasks (todo, note, status, assigned_to, related_to)
+    VALUES ('#{todo}', '#{note}', 'false', '#{assigned_to}', '#{related_to}')"
+  run_sql(sql_add_todo)
 
-  # redirect to('/people')
+  redirect to('/todos')
+end
+
+# HERE USER CAN EDIT A NEW TESK
+get '/todos/edit/:id' do
+  id = params[:id]
+  sql_get_todo = "SELECT * FROM tasks WHERE id = #{id}"
+  @got_todo = run_sql(sql_get_todo)
+
+  sql_get_movies = "SELECT * FROM movies"
+  @got_movies = run_sql(sql_get_movies)
+
+  sql_get_people = "SELECT * FROM people"
+  @got_people = run_sql(sql_get_people)
+
+  erb :edit_todo
+end
+
+# EDIT THE DATABASE AND REDIRECT
+post '/todos/edit/:id' do
+  id = params[:id]
+  todo = params[:todo].capitalize
+  note = params[:note].capitalize
+  status = params[:status]
+  assigned_to = params[:assigned_to]
+  related_to = params[:related_to]
+
+  sql_edit_todo = "UPDATE tasks SET (todo, note, status, assigned_to, related_to) = ('#{todo}', '#{note}', '#{status}', '#{assigned_to}', '#{related_to}') WHERE id = #{id}"
+  run_sql(sql_edit_todo)
+
+  redirect to("/todos/#{id}")
+end
+
+# HERE USER CAN DELETE A TASK
+  post '/todos/delete/:id' do
+  id = params[:id]
+
+  sql_delete_todo = "DELETE FROM tasks WHERE id = #{id}"
+  run_sql(sql_delete_todo)
+
+  redirect to('/todos')
 end
 
 # SHOW DETAILS OF EACH TASK
@@ -221,9 +273,19 @@ get '/todos/:id' do
   id = params[:id]
   sql_get_todo = "SELECT * FROM tasks WHERE id = #{id}"
   @got_todo = run_sql(sql_get_todo)
+
+  assignee_id = @got_todo.first['assigned_to']
+  sql_get_person = "SELECT * FROM people WHERE id = #{assignee_id}"
+  got_person = run_sql(sql_get_person)
+  @assignee = got_person.first['name']
+
+  related_movie_id = @got_todo.first['related_to']
+  sql_get_movie = "SELECT * FROM movies WHERE id = #{related_movie_id}"
+  got_movie = run_sql(sql_get_movie)
+  @for_movie = got_movie.first['title']
+
   erb :todo
 end
-
 
 
 
