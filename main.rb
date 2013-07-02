e471d0e45f7574796f0c52d55b4e2301f743b4d9
tommyb67 <tommybrennan@gmail.com>
2013-07-02 12:55:37 -0400

    I did a small bit of css, I can't call it design.  I am just showing that I can apply some styles

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require 'pg'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+
 helpers do
   # This helps run sql commands
   def run_sql(sql)
@@ -18,12 +19,15 @@ get '/' do
   erb :index
 end
 
+# This displays all of the current tasks in a bulleted list
+# 3 tables were created in psql: tasks, people and movies
 get '/todos' do
   sql = "SELECT * FROM tasks"
   @tasks = run_sql(sql)
   erb :todos
 end
 
+# Here you are able to add new tasks and add them to a person and a movie
 get '/todo/new' do
   sql = "SELECT * FROM people"
   @people = run_sql(sql)
@@ -32,6 +36,7 @@ get '/todo/new' do
   erb :todo_new
 end
 
+# This creates and new task and send it to the DB, then displays it on the todos page
 post '/todo/new' do
   task = params[:task]
   description = params[:description]
@@ -42,19 +47,27 @@ post '/todo/new' do
   redirect to '/todos'
 end
 
+# trying to pull movies by director.  I canl;t get the sql right
+get '/director' do
+sql = select * from
+end
 
+# this pulls the people list from DB
 get '/people' do
   sql = "SELECT * FROM people"
   @people = run_sql(sql)
   erb :people
 end
 
+# Tryinf to add a new person.  Form is live, but not submitting to DB
 get '/people/new' do
   sql = "SELECT id, person_name FROM people"
   @movies = run_sql(sql)
   erb :person_new
 end
 
+
+# Pulling movies from th DB
 get '/movies' do
   sql = "SELECT * FROM movies"
   @movies = run_sql(sql)
