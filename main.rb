5ab29bf84fcf023a2ee840f9449e61b0842c8407
Wesley Carr <surfswell17@gmail.com>
2013-07-01 20:01:00 -0400

    Messed around with index css... sorry

--- main.rb
+++ main.rb
@@ -1,4 +1,27 @@
-require 'sinatra'
-require 'sinatra-reloader'
 require 'pg'
 require 'imdb'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+#Begin Homework
+helpers do
+  # This helps us run SQL commands
+  def run_sql(sql)
+    db = PG.connect(:dbname => 'westeros', :host => 'localhost')
+    result = db.exec(sql)
+    db.close
+    result
+  end
+end
+
+#Routing
+get '/' do
+  erb :index
+end
+
+get '/home' do
+  erb :home
+end
+
+get '/people' do
+end
