fb3bcee0460e9030f479450005976a44ac83e88c
Tim <tim.blonski@gmail.com>
2013-07-02 18:49:09 -0400

    add get commands to see list of tasks and movies

--- main.rb
+++ main.rb
@@ -11,19 +11,43 @@ require 'pg'
   result
   end
 
-
+#see list of all todos
 get '/todos' do
   sql = "SELECT * FROM tasks"
   @tasks = run_sql(sql)
   erb :todos
 end
 
+#see individual todo
+get '/todos/:id' do
+  id = params[:id]
+  task_name = params[:task_name]
+  description = params[:description]
+  movie_id = params[:movie_id]
+  sql = "SELECT * FROM tasks WHERE id = #{id}"
+  @todo = run_sql(sql).first
+  erb :todo
+end
 
-get '/' do
+#see list of all movies
+get '/movies' do
+  sql = "SELECT * FROM movies"
+  @movies = run_sql(sql)
   erb :movies
 end
 
+#see individual movie
+get '/movies/:id' do
+  id = params[:id]
+  movie_name = params[:movie_name]
+  release = params[:release]
+  director = params[:director]
+  sql = "SELECT * FROM movies WHERE id = #{id}"
+  @movie = run_sql(sql).first
+  erb :movie
+end
 
+#see list of all people
 get '/people' do
   sql = "SELECT * FROM people"
   @people = run_sql(sql)
