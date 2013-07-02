4ecb87653f1228e4ed6ab80cb31875c5df081d80
Tim <tim.blonski@gmail.com>
2013-07-02 19:06:09 -0400

    added create new person, todo, movie functionality via get/post URLs and 3 separate ERB files.

--- main.rb
+++ main.rb
@@ -29,12 +29,48 @@ get '/todos/:id' do
   erb :todo
 end
 
+#add a new todo
+get '/todos/new' do
+  sql = "SELECT id, movie_name FROM movies"
+  @movies = run_sql(sql)
+  erb :new_todo
+end
+
+#post new todo to database
+post '/todos/new' do
+  id = params[:id]
+  task_name = params[:task_name]
+  description = params[:description]
+  movie_id = params[:movie_id]
+  sql = "INSERT INTO tasks (task_name, description, movie_id) VALUES ('#{task_name}', '#{description}','#{movie_id}');"
+  run_sql(sql)
+  redirect to '/todos'
+end
+
+
 #see list of all movies
 get '/movies' do
   sql = "SELECT * FROM movies"
   @movies = run_sql(sql)
   erb :movies
 end
+
+#get/create new movie
+get '/movies/new' do
+  erb :new_movie
+end
+
+#post new movie to the database
+post '/movies/new' do
+  id = params[:id]
+  movie_name = params[:movie_name]
+  release = params[:release]
+  director = params[:director]
+  sql = "INSERT INTO movies (movie_name, release, director) VALUES ('#{movie_name}',#{release},'#{director}')"
+  run_sql(sql)
+  redirect to '/movies'
+end
+
 
 #see individual movie
 get '/movies/:id' do
@@ -52,5 +88,25 @@ get '/people' do
   sql = "SELECT * FROM people"
   @people = run_sql(sql)
   erb :people
+end
+
+#create new person
+get '/people/new' do
+  sql = "SELECT id, movie_name FROM movies"
+  @movies = run_sql(sql)
+  sql = "SELECT id, task_name FROM tasks"
+  @todos = run_sql(sql)
+  erb :new_person
+end
+
+#post new person to database
+post '/people/new' do
+  id = params[:id]
+  person = params[:person]
+  movie_id = params[:movie_id]
+  task_id = params[:task_id]
+  sql = "INSERT INTO people (person, movie_id, task_id) VALUES ('#{person}', '#{movie_id}','#{task_id}');"
+  run_sql(sql)
+  redirect to '/people'
 end
 
