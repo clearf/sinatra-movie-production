1aac59e238991e99651f7459f513fddba6db1270
Christopher Wright <cmwright33@gmail.com>
2013-07-08 01:21:43 -0400

    reverted main.rb back to pre-ActiveRecord

--- main.rb
+++ main.rb
@@ -2,30 +2,15 @@ require 'pry'
 require 'sinatra'
 require 'pg'
 require 'sinatra/reloader' if development?
-require 'sinatra/activerecord'
 
-
-set :database, {adapter: "postgresql",
-                database: "movie_production2",
-                host: "localhost"}
-
-
-class Todos < ActiveRecord::Base
-  belongs_to :movie
-  belongs_to :people
+helpers do
+  def run_sql(sql)
+    db = PG.connect(:dbname => 'movie_production', :host => 'localhost')
+    result = db.exec(sql)
+    db.close
+    result
+  end
 end
-
-class Movies < ActiveRecord::Base
-  has_many :people
-  has_many :todos
-end
-
-class People < ActiveRecord::Base
-  belongs_to :movie
-  has_many :todos
-end
-
-
 
 # this provides you with the index page that links to each seperate page
 get '/' do
@@ -39,49 +24,52 @@ end
 # gets all the tasks
 
 get '/todos' do
-  @todos = Todos.all
+  sql = "select * from todo"
+  @todos = run_sql(sql)
 erb :todos
 end
 
 # gives each individual task
 
 get '/todo/:id' do
-  @todo = Todos.find(params[:id])
+  id = params[:id]
+  sql = "select * from todo where id = #{id}"
+  @todo = run_sql(sql).first
 erb :todo
 end
 
 # goes to the edit task
 get '/edit_todo/:id/' do
-  @todo = Todos.find(params[:id])
+sql = "select * from todo where id = #{id}"
+@todos = run_sql(sql).first
 erb :edit_todo
 end
 
 # posts the update back
 post '/edit_todo/:id/' do
-  todo = Todos.find(params[:id])
-  todo.task = params[:task]
-  todo.task_description = params[:task_description]
-  todo.movie_id = params[:movie_id]
-  todo.contact_id = params[:contact_id]
-  todo.save
-  redirect to "/todos/#{todo.id}"
+  sql = "update people set (task, task_description, person_id, movie_id) = ('#{task}', '#{task_description}', #{person_id}, #{movie_id}) WHERE id = #{id}"
+  @todos =run_sql(sql)
 end
 
 
 # create and assign new todo
 get '/create_todo' do
-  @people = People.find(params[:id])
-  @movies = Movies.find(params[:id])
+  sql = "select id, name from people"
+  @people = run_sql(sql)
+  sql = "select id, movie_name from movies"
+  @movies = run_sql(sql)
   erb :create_todo
 end
 
 # posts the task back to
 post '/create_todo' do
-  todo = Todos.create(params)
-  redirect to "/todo/#{guitar.id}"
+  task = params[:task]
+  task_description = params[:task_description]
+  sql = "INSERT INTO todo (task, task_description, person_id, movie_id) VALUES ('#{task}','#{task_description}', #{person_id}, #{movie_id});"
+  @todos = run_sql(sql)
+  redirect to '/'
   erb :todos
 end
-
 
 
 #         #
@@ -91,7 +79,8 @@ end
 # get all the people
 
 get'/people' do
-  @people = People.all
+  sql = "select * from people"
+  @people = run_sql(sql)
   erb :people
 end
 
@@ -99,23 +88,23 @@ end
 
 
 get '/person/:id' do
-  @people = People.find(params[:id])
+  id = params[:id]
+  sql = "select * from people where id = #{id}"
+  @person = run_sql(sql).first
 erb :person
 end
 
 
  # gives you the person information to edit
  get '/edit_person/:id' do
-
-  @people = People.find(params[:id])
+  sql = "select * from people where id = #{id}"
+  @person = run_sql(sql).first
  end
 
 #  posts the edits back to person
 post '/edit_person/:id' do
-  people = People.find(params[:id])
-  people.name = params[:name]
-  people.occupation = params[:occupation]
-  people.save
+  sql = "update people set (name, occupation) = ('#{name}', '#{occupation}') WHERE id = #{id}"
+  @person = run_sql(sql).first
 end
 
 
@@ -127,7 +116,11 @@ end
 
 # posts the person back to people
 post '/create_person' do
-  person = People.create(params)
+  name = params[:name]
+  occupation = params[:occupation]
+  sql = "insert into people (name, occupation) values ('#{name}', '#{occupation}')"
+  run_sql(sql)
+  erb :people
 end
 
 
@@ -140,14 +133,17 @@ end
 # gets the movie
 
 get '/movies' do
-  movie = Movies.all
+  sql = "select * from movies"
+  @movies = run_sql(sql)
   erb :movies
 end
 
 # gets single movie with information
 
 get '/movie/:id' do
-  movie = Movies.find(params[:id])
+  id = params[:id]
+  sql =  "select * from movies where id = #{id}"
+  @movie = run_sql(sql).first
   erb :movie
 end
 
@@ -158,7 +154,11 @@ end
 
 # posts the person back to people
 post '/create_movie' do
-  movie = Movies.create(params)
+  movie_name = params[:movie_name]
+  release_date = params[:release_date]
+  director = params[:director]
+  sql = "insert into movies (movie_name, release_date, director) values ('#{movie_name}', #{release_date}, '#{director}')"
+  run_sql(sql)
 end
 
 get '/edit_movie/:id' do
@@ -167,12 +167,12 @@ erb :edit_movie
 end
 
 post '/edit_movie/:id' do
-  movie = Movies.find(params[:id])
-  movie.movie_name = params[:movie_name]
-  movie.release_date = params[:release_date]
-  movie.director = params[:director]
-  movie.save
-  redirect to "/movies/#{movie.id}"
+  id = params[:id]
+  @movie_name = params[:movie_name]
+  @release_date = params[:release_date]
+  @director = params[:director]
+  sql = "update movies set (movie_name, release_date, director) values ('#{movie_name}', #{release_date}, '#{director}') WHERE id = #{id}"
+  @person = run_sql(sql).first
 end
 
 
